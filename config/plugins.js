699f12ca931f057f0313e9ebdbbdb8cb3480b113
esdevelopers1909 <esdevelopers1909@gmail.com>
2024-09-20 23:54:11 +0530

    ekeditor plugin

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+    ckeditor: {
+      enabled: true,
+      config: {
+        // Additional CKEditor configuration options
+        plugin: {
+          // Plugin configurations (optional)
+        },
+        editor: {
+          // Editor configurations
+        }
+      }
+    }
+  });
+  
