b1d56f7e5663b72090042d1dee7a4dbd267ded49
esdevelopers1909 <esdevelopers1909@gmail.com>
2024-09-21 01:53:03 +0530

    added admin.js file

--- config/admin.ts
+++ config/admin.ts
@@ -3,7 +3,7 @@ export default ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', "0NrmEX5u3m1FDKKeVynFiA=="),
   },
   transfer: {
     token: {
