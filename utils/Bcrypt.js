5f55d12cec42ab37f43fd598f0aac2fd91fa6ed9
heyakhil <luvprogramming@gmail.com>
2024-12-06 23:53:56 +0530

    fix: removed console.log

--- utils/Bcrypt.js
+++ utils/Bcrypt.js
@@ -2,12 +2,6 @@ const crypto = require("crypto");
 const hashString = (str) => {
   console.log(str);
   const hashedString = crypto.createHash("sha256").update(str).digest("hex");
-  console.log(
-    crypto
-      .createHash("sha256")
-      .update("/articles/category-1/415379284500-test")
-      .digest("hex")
-  );
   return hashedString;
 };
 
